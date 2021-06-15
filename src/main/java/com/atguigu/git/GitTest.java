1a3797f6649fb68ea18611da43808722aac34ab5
wangjiandong <1175792154@qq.com>
2021-06-15 16:15:19 +0800

    first commit!

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wangjiandong
+ * @create 2021-06-15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
